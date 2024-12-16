ea8f4475a776637a4406b489c9a2298641474b4b
Matija Cvetan <matija.cvetan28@gmail.com>
2024-12-16 07:33:19 +0100

    Day15 Part 2 Partial

---
+++ Day15/Coordinate.cs
@@ -0,0 +1,23 @@
+﻿namespace Day15
+{
+    public struct Coordinate(long x, long y)
+    {
+        public long X { get; set; } = x;
+        public long Y { get; set; } = y;
+
+        public override bool Equals(object obj) =>
+            obj is Coordinate coordinate &&
+                   X == coordinate.X &&
+                   Y == coordinate.Y;
+
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+
+        public override string ToString() => $"({X}, {Y})";
+
+        public static bool operator ==(Coordinate left, Coordinate right) =>
+            left.Equals(right);
+
+        public static bool operator !=(Coordinate left, Coordinate right) =>
+            !(left == right);
+    }
+}
--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 
-string filePathGrid = "grid.txt";
-string filePathInput = "input.txt";
+string filePathGrid = "testGrid.txt";
+string filePathInput = "testInput.txt";
 
 char[][] grid = (await File.ReadAllLinesAsync(filePathGrid))
     .Select(line => line.ToCharArray())
@@ -18,17 +18,147 @@ foreach (var line in input)
     }
 }
 
-int gridLegth = grid.Length;
-int gridWidth = grid[0].Length;
-var boxes = 'O';
-var robot = '@';
-var gridLimit = '#';
+const char boxes = 'O';
+const char robot = '@';
+const char gridLimit = '#';
+const char newBoxRight = '[';
+const char newBoxLeft = ']';
+const char emptySpace = '.';
 
 var robotPosition = FindCharPosition(grid, robot);
 var boxPositions = FindCharPositions(grid, boxes);
 var gridLimitPositions = FindCharPositions(grid, gridLimit);
 
-bool CanMoveBoxes(Coordinate currentPosition, Coordinate direction, Collection<Coordinate> boxPositions, Collection<Coordinate> gridLimitPositions)
+
+// Part 1
+foreach (var direction in directions)
+{
+    var nextPosition = new Coordinate(
+        robotPosition.X + direction.DirectionCoordinates.X,
+        robotPosition.Y + direction.DirectionCoordinates.Y
+    );
+
+    if (gridLimitPositions.Contains(nextPosition))
+    {
+        continue;
+    }
+
+    if (boxPositions.Contains(nextPosition) &&
+        CanMoveBoxes(nextPosition, direction.DirectionCoordinates, boxPositions, gridLimitPositions))
+    {
+        MoveBoxes(nextPosition, direction.DirectionCoordinates, boxPositions);
+    }
+    else if (boxPositions.Contains(nextPosition))
+    {
+        continue;
+    }
+
+    robotPosition = nextPosition;
+}
+
+var partOneResult = boxPositions.Select(pos => 100 * pos.X + pos.Y).Sum();
+
+Console.WriteLine(partOneResult);
+
+// Part 2
+var newGrid = TransformGrid(grid);
+
+var robotPosition2 = FindCharPosition(newGrid, robot);
+var gridLimitPositions2 = FindCharPositions(newGrid, gridLimit);
+var boxPositions2 = FindCharPositions(newGrid, newBoxRight, newBoxLeft);
+var boxPairs = FindBoxCooridinates(grid);
+
+foreach (var direction in directions)
+{
+    var nextPosition = new Coordinate(
+        robotPosition2.X + direction.DirectionCoordinates.X,
+        robotPosition2.Y + direction.DirectionCoordinates.Y
+    );
+
+    if (gridLimitPositions2.Contains(nextPosition))
+    {
+        continue;
+    }
+
+    if (boxPairs.ContainsKey(nextPosition) &&
+        (direction.DirectionChar == '>' ||
+        direction.DirectionChar == '<') &&
+        CanMoveBoxes(nextPosition, direction.DirectionCoordinates, boxPositions2, gridLimitPositions2))
+    {
+        MoveBoxes2(nextPosition, direction.DirectionCoordinates, boxPositions2);
+    }
+    else if (boxPairs.ContainsKey(nextPosition) &&
+        (direction.DirectionChar == 'v' ||
+        direction.DirectionChar == '^') &&
+        CanMoveBoxesVertical(nextPosition, direction.DirectionCoordinates, gridLimitPositions2))
+    {
+        MoveBoxesVertical(nextPosition, direction.DirectionCoordinates);
+    }
+    else if (boxPairs.ContainsKey(nextPosition))
+    {
+        continue;
+    }
+
+    robotPosition2 = nextPosition;
+    //
+    Console.WriteLine(direction.DirectionChar);
+    var newGridTest = CreateNewGrid(robotPosition2, gridLimitPositions2, boxPositions2, newGrid.Length, newGrid[0].Length);
+    PrintGrid(newGridTest);
+    foreach (var box in boxPairs)
+    {
+        Console.WriteLine($"{box.Key.X}:{box.Key.Y}  => {box.Value.X}:{box.Value.Y}");
+    }
+    //
+}
+
+var partTwoResult = boxPositions2.Where(b => b.Equals(newBoxLeft)).Select(pos => 100 * pos.X + pos.Y).Sum();
+
+Console.WriteLine(partTwoResult);
+
+////////
+char[][] CreateNewGrid(
+   Coordinate robotPosition,
+   Collection<Coordinate> gridLimitPositions,
+   Collection<Coordinate> boxPositions,
+   int rows,
+   int cols)
+{
+    // Create a new grid filled with '.'
+    char[][] newGrid = new char[rows][];
+    for (int i = 0; i < rows; i++)
+    {
+        newGrid[i] = new char[cols];
+        Array.Fill(newGrid[i], '.');
+    }
+
+    // Place robot
+    newGrid[robotPosition.X][robotPosition.Y] = '@';
+
+    // Place grid limits
+    foreach (var limitPos in gridLimitPositions)
+    {
+        newGrid[limitPos.X][limitPos.Y] = '#';
+    }
+
+    // Place boxes
+    foreach (var boxPos in boxPositions)
+    {
+        newGrid[boxPos.X][boxPos.Y] = 'O';
+    }
+
+    return newGrid;
+}
+
+// Method to print the grid to console
+void PrintGrid(char[][] grid)
+{
+    foreach (var row in grid)
+    {
+        Console.WriteLine(new string(row));
+    }
+}
+//////////
+bool CanMoveBoxes(Coordinate currentPosition, Coordinate direction, Collection<Coordinate> boxPositionss, Collection<Coordinate> gridLimitPositions)
 {
     var nextPosition = new Coordinate(
         currentPosition.X + direction.X,
@@ -40,7 +170,7 @@ bool CanMoveBoxes(Coordinate currentPosition, Coordinate direction, Collection<C
         return false;
     }
 
-    if (boxPositions.Contains(nextPosition))
+    if (boxPositionss.Contains(nextPosition))
     {
         return CanMoveBoxes(nextPosition, direction, boxPositions, gridLimitPositions);
     }
@@ -64,35 +194,155 @@ void MoveBoxes(Coordinate currentPosition, Coordinate direction, Collection<Coor
     boxPositions.Add(nextPosition);
 }
 
-foreach (var direction in directions)
+void MoveBoxes2(Coordinate currentPosition, Coordinate direction, Collection<Coordinate> boxPositions, int count = 0)
 {
     var nextPosition = new Coordinate(
-        robotPosition.X + direction.DirectionCoordinates.X,
-        robotPosition.Y + direction.DirectionCoordinates.Y
+        currentPosition.X + direction.X,
+        currentPosition.Y + direction.Y
     );
+    count++;
 
-    if (gridLimitPositions.Contains(nextPosition))
+    if (boxPairs.ContainsKey(nextPosition))
     {
-        continue;
+        MoveBoxes2(new Coordinate(currentPosition.X, currentPosition.Y + direction.Y), direction, boxPositions, count);
     }
 
-    if (boxPositions.Contains(nextPosition) &&
-        CanMoveBoxes(nextPosition, direction.DirectionCoordinates, boxPositions, gridLimitPositions))
-    {
-        MoveBoxes(nextPosition, direction.DirectionCoordinates, boxPositions);
-    }
-    else if(boxPositions.Contains(nextPosition))
-    {
-        continue;
-    }
+    boxPositions2.Remove(currentPosition);
+    boxPositions2.Add(nextPosition);
 
-    robotPosition = nextPosition;
+    boxPairs.Remove(currentPosition);
+    boxPairs.Add(nextPosition, count % 2 == 0 ? new Coordinate(currentPosition.X, currentPosition.Y) : new Coordinate(nextPosition.X, nextPosition.Y + direction.Y));
 }
 
-var partOneResult = boxPositions.Select(pos => 100 * pos.X + pos.Y).Sum();
+void MoveBoxesVertical(Coordinate currentPosition, Coordinate direction)
+{
 
-Console.WriteLine(partOneResult);
+    var positionRight = new Coordinate(
+        currentPosition.X,
+        currentPosition.Y + 1
+    );
 
+    var positionLeft = new Coordinate(
+        currentPosition.X,
+        currentPosition.Y - 1
+     );
+
+    var nextPositionOne = new Coordinate(
+        currentPosition.X + direction.X,
+        currentPosition.Y + direction.Y
+    );
+
+    var nextPositionRight = new Coordinate(
+        positionRight.X + direction.X,
+        positionRight.Y + direction.Y);
+
+    var nextPositionLeft = new Coordinate(
+        currentPosition.X + direction.X,
+        currentPosition.Y + direction.Y - 1
+ );
+
+    if (boxPairs.TryGetValue(currentPosition, out var valueCurrent) && valueCurrent.Equals(positionRight) && boxPairs.ContainsKey(nextPositionRight))
+    {
+        MoveBoxesVertical(nextPositionRight, direction);
+    }
+    if (boxPairs.ContainsKey(nextPositionLeft) && valueCurrent.Equals(positionLeft))
+    {
+        MoveBoxesVertical(nextPositionLeft, direction);
+    }
+    if (boxPairs.ContainsKey(nextPositionOne) &&
+        (boxPairs.TryGetValue(nextPositionLeft, out var valueLeft) && valueLeft.Equals(nextPositionOne) ||
+        boxPairs.TryGetValue(nextPositionRight, out var valueRight) && valueRight.Equals(nextPositionOne)))
+    {
+        MoveBoxesVertical(nextPositionOne, direction);
+    }
+
+    var isRight = valueCurrent.Equals(positionRight);
+
+    boxPairs.Remove(currentPosition);
+    boxPairs.Remove(isRight ? positionRight : positionLeft);
+    boxPairs.Add(nextPositionOne, isRight ? nextPositionRight : nextPositionLeft);
+    boxPairs.Add(isRight ? nextPositionRight : nextPositionLeft, nextPositionOne);
+
+    boxPositions2.Remove(currentPosition);
+    boxPositions2.Remove(isRight ? positionRight : positionLeft);
+    boxPositions2.Add(nextPositionOne);
+    boxPositions2.Add(isRight ? nextPositionRight : nextPositionLeft);
+}
+
+bool CanMoveBoxesVertical(Coordinate currentPosition, Coordinate direction, Collection<Coordinate> gridLimitPositions)
+{
+    var nextPositionOne = new Coordinate(
+        currentPosition.X + direction.X,
+        currentPosition.Y + direction.Y
+    );
+
+    var nextPositionRight = new Coordinate(
+        currentPosition.X + direction.X,
+        currentPosition.Y + direction.Y + 1
+    );
+
+    var nextPositionLeft = new Coordinate(
+            currentPosition.X + direction.X,
+            currentPosition.Y + direction.Y - 1
+        );
+
+    if (gridLimitPositions.Contains(nextPositionOne) || gridLimitPositions.Contains(nextPositionRight) || gridLimitPositions.Contains(nextPositionLeft))
+    {
+        return false;
+    }
+
+    if (boxPairs.ContainsKey(nextPositionOne))
+    {
+        return CanMoveBoxesVertical(nextPositionOne, direction, gridLimitPositions);
+    }
+    if (boxPairs.ContainsKey(nextPositionRight))
+    {
+        return CanMoveBoxesVertical(nextPositionRight, direction, gridLimitPositions);
+    }
+    if (boxPairs.ContainsKey(nextPositionLeft))
+    {
+        return CanMoveBoxesVertical(nextPositionLeft, direction, gridLimitPositions);
+    }
+
+    return true;
+}
+
+char[][] TransformGrid(char[][] grid)
+{
+    int rows = grid.Length;
+
+    char[][] newGrid = new char[rows][];
+
+    for (int i = 0; i < rows; i++)
+    {
+        newGrid[i] = new char[grid[i].Length * 2];
+
+        for (int j = 0; j < grid[i].Length; j++)
+        {
+            switch (grid[i][j])
+            {
+                case gridLimit:
+                    newGrid[i][j * 2] = gridLimit;
+                    newGrid[i][j * 2 + 1] = gridLimit;
+                    break;
+                case boxes:
+                    newGrid[i][j * 2] = newBoxRight;
+                    newGrid[i][j * 2 + 1] = newBoxLeft;
+                    break;
+                case robot:
+                    newGrid[i][j * 2] = robot;
+                    newGrid[i][j * 2 + 1] = emptySpace;
+                    break;
+                default:
+                    newGrid[i][j * 2] = emptySpace;
+                    newGrid[i][j * 2 + 1] = emptySpace;
+                    break;
+            }
+        }
+    }
+
+    return newGrid;
+}
 
 Coordinate FindCharPosition(char[][] grid, char c)
 {
@@ -109,20 +359,42 @@ Coordinate FindCharPosition(char[][] grid, char c)
     return new Coordinate(-1, -1);
 }
 
-Collection<Coordinate> FindCharPositions(char[][] grid, char c)
+Collection<Coordinate> FindCharPositions(char[][] grid, params char[] chars)
 {
     var positions = new Collection<Coordinate>();
     for (int i = 0; i < grid.Length; i++)
     {
         for (int j = 0; j < grid[i].Length; j++)
         {
-            if (grid[i][j] == c)
+            if (chars.Contains(grid[i][j]))
             {
                 positions.Add(new Coordinate(i, j));
             }
         }
     }
-    return [.. positions];
+    return positions;
+}
+
+Dictionary<Coordinate, Coordinate> FindBoxCooridinates(char[][] grid)
+{
+    var mapping = new Dictionary<Coordinate, Coordinate>();
+
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[i].Length; j++)
+        {
+            if (grid[i][j] == boxes)
+            {
+                var coord1 = new Coordinate(i, j * 2);
+                var coord2 = new Coordinate(i, j * 2 + 1);
+
+                mapping[coord1] = coord2;
+                mapping[coord2] = coord1;
+            }
+        }
+    }
+
+    return mapping;
 }
 
 
@@ -141,10 +413,19 @@ struct Direction(char direction)
         Left => new Coordinate(0, -1),
         _ => new Coordinate(0, 0)
     };
+
+    public char DirectionChar { get; set; } = direction;
 }
 
 struct Coordinate(long x, long y)
 {
     public long X { get; set; } = x;
     public long Y { get; set; } = y;
+
+    public override bool Equals(object obj) =>
+        obj is Coordinate coordinate &&
+               X == coordinate.X &&
+               Y == coordinate.Y;
+
+    public override int GetHashCode() => HashCode.Combine(X, Y);
 }
